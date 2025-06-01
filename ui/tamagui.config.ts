e9b2e23e68c185ad0546a16e0c8c4140544d422a
kachdekan <kachdekan@gmail.com>
2025-06-01 20:06:33 +0300

    fix: chande fonts again

--- ui/tamagui.config.ts
+++ ui/tamagui.config.ts
@@ -1,5 +1,6 @@
-import { animations, media, settings, shorthands } from "@tamagui/config/v4";
+import { media, settings, shorthands } from "@tamagui/config/v4";
 import { createTamagui } from "@tamagui/core";
+import { animations } from "./theme/animations";
 import { allFonts } from "./theme/fonts";
 import { themes } from "./theme/themes";
 import { tokens } from "./theme/tokens";
