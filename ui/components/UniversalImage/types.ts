fc64afee8f86f9424d610fd6602c7b63ad157066
kachdekan <kachdekan@gmail.com>
2025-07-09 19:16:35 +0300

    fix: replace fast-image with expo image

--- ui/components/UniversalImage/types.ts
+++ ui/components/UniversalImage/types.ts
@@ -1,67 +1,67 @@
-import type { ImageRequireSource } from 'react-native'
-import type { StackProps } from 'tamagui'
+import type { ImageRequireSource } from "react-native";
+import type { StackProps } from "tamagui";
 
 export interface UniversalImageStyle {
-  backgroundColor?: string
-  borderRadius?: number
-  verticalAlign?: StackProps['verticalAlign']
-  zIndex?: number
+	backgroundColor?: string;
+	borderRadius?: number;
+	verticalAlign?: StackProps["verticalAlign"];
+	zIndex?: number;
 }
 
 export enum UniversalImageResizeMode {
-  Center = 'center',
-  Contain = 'contain',
-  Cover = 'cover',
-  Stretch = 'stretch',
+	Center = "center",
+	Contain = "contain",
+	Cover = "cover",
+	Stretch = "stretch",
 }
 
 export interface UniversalImageStyleProps {
-  image?: UniversalImageStyle // ImageStyle
-  container?: UniversalImageStyle // ImageStyle
-  loadingContainer?: StackProps['style']
+	image?: UniversalImageStyle; // ImageStyle
+	container?: UniversalImageStyle; // ImageStyle
+	loadingContainer?: StackProps["style"];
 }
 
 interface SharedImageSizeProps {
-  width?: number
-  height?: number
-  aspectRatio?: number
+	width?: number;
+	height?: number;
+	aspectRatio?: number;
 }
 
 export type UniversalImageSize = SharedImageSizeProps & {
-  resizeMode?: UniversalImageResizeMode
-}
+	resizeMode?: UniversalImageResizeMode;
+};
 
 // Top level props
 
 export interface UniversalImageProps {
-  uri?: string | ImageRequireSource
-  size: UniversalImageSize
-  fallback?: JSX.Element
-  style?: UniversalImageStyleProps
-  fastImage?: boolean
-  testID?: string
-  allowLocalUri?: boolean
-  autoplay?: boolean
-  onLoad?: () => void
+	uri?: string | ImageRequireSource;
+	size: UniversalImageSize;
+	fallback?: JSX.Element;
+	style?: UniversalImageStyleProps;
+	fastImage?: boolean;
+	testID?: string;
+	allowLocalUri?: boolean;
+	autoplay?: boolean;
+	onLoad?: () => void;
 }
 
 export interface PlainImageProps {
-  uri: string
-  size: SharedImageSizeProps
-  fallback?: JSX.Element
-  style?: UniversalImageStyle
-  resizeMode?: UniversalImageResizeMode
-  testID?: string
-  onLoad?: () => void
+	uri: string;
+	size: SharedImageSizeProps;
+	fallback?: JSX.Element;
+	style?: UniversalImageStyle;
+	resizeMode?: UniversalImageResizeMode;
+	testID?: string;
+	onLoad?: () => void;
 }
 
 export type FastImageWrapperProps = PlainImageProps & {
-  setError: () => void
-}
+	setError: () => void;
+};
 
 export type SvgImageProps = {
-  uri: string
-  size: SharedImageSizeProps
-  autoplay: boolean
-  fallback?: JSX.Element
-}
+	uri: string;
+	size: SharedImageSizeProps;
+	autoplay: boolean;
+	fallback?: JSX.Element;
+};
