3f450c98ea8d3a203449edacd439985abfaaca8a
kachdekan <kachdekan@gmail.com>
2025-07-13 04:47:19 +0300

    feat: setup rosca creation.

---
+++ app/(spaces)/roscas/[spaceId].tsx
@@ -0,0 +1,254 @@
+import { BackButton } from "@/components/Buttons/BackButton";
+import { getSavings } from "@/features/contracts/goal-savings";
+import { getChainInfo, getRate } from "@/features/wallet";
+import { useEnabledChains } from "@/features/wallet/hooks";
+import { useWalletState } from "@/features/wallet/walletState";
+import {
+	Button,
+	IconButton,
+	LinearGradient,
+	Separator,
+	Stack,
+	Text,
+	TouchableArea,
+	View,
+	XStack,
+	YStack,
+} from "@/ui";
+import {
+	ReceiveAlt,
+	RotatableChevron,
+	SendAction,
+	Settings,
+} from "@/ui/components/icons";
+import { router, useLocalSearchParams } from "expo-router";
+import { useEffect, useState } from "react";
+import { Progress } from "tamagui";
+
+export default function SpaceHome() {
+	const params = useLocalSearchParams();
+	const currency = useWalletState((s) => s.currency);
+	const { defaultChainId } = useEnabledChains();
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const { symbol, conversionRate } = getRate(currency);
+	const chain = getChainInfo(defaultChainId);
+	const [isSloted, setIsSlotted] = useState<boolean>(false);
+
+	const [spaceInfo, setSpaceInfo] = useState({
+		spaceId: params.spaceId as string,
+		name: params.name as string,
+		startDate: Number(params.startDate),
+		targetAmount: Number(params.targetAmount),
+		memberCount: Number(params.memberCount),
+		interval: Number(params.interval),
+	});
+
+	const [slotInfo, setSlotInfo] = useState({
+		amount: 0,
+		deadline: Date.now(),
+		targetAmount: spaceInfo.targetAmount / spaceInfo.memberCount,
+		yield: 0,
+	});
+	const isTarget = slotInfo.amount === slotInfo.targetAmount;
+	const date: Date = new Date(slotInfo.deadline);
+	const balInCurreny = slotInfo.amount * conversionRate;
+	const balSplit = balInCurreny.toFixed(2).split(".");
+	const progress = Number(
+		((slotInfo.amount / slotInfo.targetAmount) * 100).toFixed(0),
+	);
+	const transactions = [];
+
+	useEffect(() => {
+		setIsLoading(true);
+		const getSpace = async () => {
+			const savings = await getSavings({
+				chainId: defaultChainId,
+				spaceId: spaceInfo.spaceId,
+			});
+			if (savings)
+				//setSpaceInfo(savings);
+				setIsLoading(false);
+		};
+		setIsLoading(false);
+		//getSpace();
+	}, [defaultChainId, spaceInfo.spaceId]);
+
+	return (
+		<View flex={1} bg="$surface1" items="center">
+			<LinearGradient
+				width="100%"
+				height="100%"
+				colors={["$surface1", "$surface3"]}
+				position="absolute"
+			/>
+			<YStack gap="$md" width="100%" items="center">
+				<XStack
+					height={265}
+					bg="$blueLight"
+					width="100%"
+					justify="space-between"
+					items="flex-end"
+					px="$xl"
+					py="$xl"
+				>
+					<Stack position="absolute" t="$lg" l="$lg">
+						<BackButton
+							color="$neutral2"
+							size={28}
+							onPressBack={() => router.replace("/(tabs)/spaces")}
+						/>
+					</Stack>
+					<YStack gap="$md">
+						<Text variant="subHeading1">{spaceInfo.name}</Text>
+						<YStack gap="$xs">
+							<Text
+								variant="heading3"
+								fontWeight="800"
+								color="$neutral1"
+								fontSize={30}
+							>
+								{`${symbol} ${balSplit[0]}`}
+								<Text
+									variant="heading3"
+									fontWeight="800"
+									color="$neutral3"
+									fontSize={30}
+								>
+									.{balSplit[1]}
+								</Text>
+							</Text>
+							<XStack items="center" gap="$2xs">
+								<Text variant="subHeading2" color="$neutral2">
+									≈ ${(slotInfo.amount - slotInfo.yield).toFixed(2)}
+								</Text>
+								<TouchableArea
+									bg="$tealThemed"
+									rounded="$full"
+									py="$4xs"
+									px="$sm"
+									onPress={() => {}}
+								>
+									<Text variant="subHeading2" color="$neutral1">
+										+{slotInfo.yield.toFixed(2)}
+									</Text>
+								</TouchableArea>
+							</XStack>
+						</YStack>
+					</YStack>
+					<IconButton
+						icon={<Settings size={24} color="$accent1" />}
+						size="md"
+						variant="branded"
+						emphasis="secondary"
+					/>
+				</XStack>
+				{isSloted && Date.now() > spaceInfo.startDate ? (
+					<YStack gap="$xs" width="92%">
+						<Text ml="$lg">Pick your slot before 28th June</Text>
+						<YStack gap="$xs" p="$sm" rounded="$md" bg="$surface1">
+							<XStack justify="space-between">
+								<YStack gap="$2xs">
+									<Text color="$neutral2" variant="body3">
+										Target payout
+									</Text>
+									<Text variant="subHeading1" color="$tealDark">
+										Ksh 10000
+									</Text>
+								</YStack>
+								<Button
+									variant="branded"
+									width="30%"
+									rounded="$full"
+									icon={<RotatableChevron direction="right" />}
+									iconPosition="after"
+								>
+									Pick
+								</Button>
+							</XStack>
+							<XStack items="center">
+								<Text mr="$sm" variant="body3">
+									Pay
+								</Text>
+								<Separator />
+							</XStack>
+							<XStack justify="space-between">
+								<Text>
+									Ksh 1000 <Text color="$neutral2">per month</Text>
+								</Text>
+								<Text>5 Free slots</Text>
+							</XStack>
+						</YStack>
+					</YStack>
+				) : (
+					<YStack
+						gap="$sm"
+						width="92%"
+						px="$sm"
+						pt="$md"
+						pb="$xl"
+						rounded="$lg"
+						bg="$surface1"
+					>
+						<XStack justify="space-between">
+							<Text fontWeight="$md">Saved ${slotInfo.amount.toFixed(2)}</Text>
+							<Text>Target: ${slotInfo.targetAmount.toFixed(2)}</Text>
+						</XStack>
+						<Progress value={progress} height="$xs" bg="$tealThemed">
+							<Progress.Indicator bg="$tealBase" animation="80ms-ease-in-out" />
+						</Progress>
+						<Text>
+							{date.toLocaleDateString("en-US", {
+								weekday: "short",
+								day: "numeric",
+								month: "short",
+								year: "numeric",
+							})}
+							{/*<Text color="$neutral2">- 1 month to go</Text>*/}
+						</Text>
+					</YStack>
+				)}
+				{/*<TransactionsCard transactions={transactions} isLoading={isLoading} />*/}
+			</YStack>
+			<XStack justify="space-between" position="absolute" b="$3xl" width="92%">
+				<Button
+					variant="branded"
+					emphasis={slotInfo.amount > 0 ? "secondary" : "primary"}
+					size="lg"
+					width={slotInfo.amount > 0 ? (isTarget ? "25%" : "48%") : "72%"}
+					icon={<SendAction size={24} />}
+					onPress={() =>
+						router.navigate({
+							pathname: "/(spaces)/add-cash",
+							params: {
+								address: chain.contracts?.goalSavings.address,
+								name: spaceInfo.name,
+								id: spaceInfo.spaceId,
+							},
+						})
+					}
+				>
+					{isTarget ? null : "Add Cash"}
+				</Button>
+				<Button
+					variant="branded"
+					emphasis={isTarget ? "primary" : "secondary"}
+					size="lg"
+					width={slotInfo.amount > 0 ? (isTarget ? "72%" : "48%") : "25%"}
+					icon={<ReceiveAlt size={24} />}
+					onPress={() =>
+						router.navigate({
+							pathname: "/(spaces)/cash-out",
+							params: {
+								address: chain.contracts?.goalSavings.address,
+								name: spaceInfo.name,
+								id: spaceInfo.spaceId,
+							},
+						})
+					}
+				>
+					{slotInfo.amount > 0 ? "Cash Out" : null}
+				</Button>
+			</XStack>
+		</View>
+	);
+}
