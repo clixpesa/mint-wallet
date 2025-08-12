4de126d79932dae227f0b5ef323f41c115b47a3b
kachdekan <kachdekan@gmail.com>
2025-08-13 00:14:55 +0300

    fix: edit rosca names

--- app/(spaces)/roscas/info.tsx
+++ app/(spaces)/roscas/info.tsx
@@ -1,11 +1,26 @@
 import { AccountIcon } from "@/components/account/AccountIcon";
 import { Screen } from "@/components/layout/Screen";
-import { getRoscaMembers } from "@/features/contracts/roscas";
-import { getChainInfo } from "@/features/wallet";
+import { editRosca, getRoscaMembers } from "@/features/contracts/roscas";
+import { getChainInfo, useWalletContext } from "@/features/wallet";
 import { useEnabledChains } from "@/features/wallet/hooks";
-import { ScrollView, Text, TouchableArea, XStack, YStack } from "@/ui";
+import {
+	Button,
+	ScrollView,
+	Text,
+	TextInput,
+	TouchableArea,
+	XStack,
+	YStack,
+} from "@/ui";
 import { Edit, Logout, Search, SendAction } from "@/ui/components/icons";
 import { isSameAddress, shortenAddress } from "@/utilities/addresses";
+import {
+	collection,
+	getDocs,
+	getFirestore,
+	query,
+	where,
+} from "@react-native-firebase/firestore";
 import { useLocalSearchParams } from "expo-router";
 import { useEffect, useState } from "react";
 import { Alert } from "react-native";
@@ -15,7 +30,16 @@ export default function GroupInfo() {
 	const params = useLocalSearchParams();
 	const { defaultChainId } = useEnabledChains();
 	const chain = getChainInfo(defaultChainId);
+	const [edit, setEdit] = useState<boolean>(false);
+	const [name, setName] = useState<string>(params.name as string);
 	const [members, setMembers] = useState<Address[]>([]);
+	const { updateCurrentChainId, mainAccount } = useWalletContext();
+	const [userMap, setUserMap] = useState<
+		Map<
+			Address,
+			{ tag: string; evmAddress: string; displayName: string | null } | null
+		>
+	>(new Map());
 	const [isLoading, setIsLoading] = useState<boolean>(false);
 	useEffect(() => {
 		setIsLoading(true);
@@ -26,11 +50,73 @@ export default function GroupInfo() {
 			});
 
 			if (members) setMembers(members);
+			const userMap = await getMemberDetails(members);
+			setUserMap(userMap);
 			setIsLoading(false);
 		};
 		setIsLoading(false);
 		getSpace();
 	}, [defaultChainId]);
+
+	const onCancel = () => {
+		//setDate(new Date(Number(params.deadline)));
+		setName(params.name as string);
+		//setGoal(params.goal as string);
+	};
+	const onPressSave = async () => {
+		setIsLoading(true);
+		if (mainAccount) {
+			const reciept = await editRosca({
+				account: mainAccount,
+				chainId: defaultChainId,
+				name: name,
+				//targetAmount: goal,
+				//targetDate: date.valueOf(),
+				spaceId: params.spaceId as string,
+			});
+			console.log(reciept);
+			setIsLoading(false);
+		}
+	};
+
+	useEffect(() => {
+		updateCurrentChainId(defaultChainId);
+	}, [defaultChainId, updateCurrentChainId]);
+
+	const getMemberDetails = async (members: Address[]) => {
+		const batchSize = 30;
+		const usersCollection = collection(getFirestore(), "USERS");
+		const userMap = new Map<
+			Address,
+			{ tag: string; evmAddress: string; displayName: string | null } | null
+		>();
+		for (let i = 0; i < members.length; i += batchSize) {
+			const batch = members.slice(i, i + batchSize);
+
+			const querySnapshot = await getDocs(
+				query(usersCollection, where("customClaims.evmAddr", "in", batch)),
+			);
+
+			const foundUsers = new Map<
+				Address,
+				{ tag: string; evmAddress: string; displayName: string | null }
+			>();
+			querySnapshot.forEach((doc) => {
+				const user = doc.data();
+				if (user.customClaims?.evmAddr) {
+					foundUsers.set(user.customClaims.evmAddr, {
+						tag: user.customClaims.tag,
+						evmAddress: user.customClaims.evmAddr,
+						displayName: user.displayName,
+					});
+				}
+			});
+			batch.forEach((Addr) => {
+				userMap.set(Addr, foundUsers.get(Addr) || null);
+			});
+		}
+		return userMap;
+	};
 	return (
 		<Screen>
 			<ScrollView showsVerticalScrollIndicator={false} width="100%">
@@ -54,6 +140,7 @@ export default function GroupInfo() {
 									borderWidth={2}
 									borderColor="$surface3Hovered"
 									rounded="$md"
+									onPress={() => setEdit(!edit)}
 								>
 									<Edit size={24} m="$xs" color="$neutral2" />
 								</TouchableArea>
@@ -81,36 +168,78 @@ export default function GroupInfo() {
 							</Text>
 						</YStack>
 					</XStack>
-					<YStack gap="$2xs" mx="$xl">
-						<Text variant="subHeading1">{params.name}</Text>
-					</YStack>
+					{edit ? null : (
+						<YStack gap="$2xs" mx="$xl">
+							<Text variant="subHeading1">{name}</Text>
+						</YStack>
+					)}
+					{edit ? (
+						<XStack justify="space-between" items="center">
+							<TextInput
+								width="100%"
+								placeholder={name}
+								value={name}
+								onChangeText={(text) => setName(text)}
+							/>
+						</XStack>
+					) : null}
+					{edit ? (
+						<XStack gap="$md" self="center" mt="$xl">
+							<Button
+								variant="branded"
+								emphasis="secondary"
+								width="30%"
+								onPress={() => {
+									onCancel();
+								}}
+							>
+								Cancel
+							</Button>
+							<Button
+								variant="branded"
+								width="30%"
+								loading={isLoading}
+								onPress={() => {
+									setEdit(!edit);
+									onPressSave();
+								}}
+							>
+								{isLoading ? "Saving " : "Save"}
+							</Button>
+						</XStack>
+					) : null}
 					<XStack justify="space-between" items="center" mx="$lg" mt="$xl">
 						<Text>{members.length} members</Text>
 						<Search size={22} color="$neutral1" />
 					</XStack>
 					<YStack bg="$surface1" p="$sm" rounded="$lg" gap="$md">
-						{members.map((member, index) => (
-							<XStack items="center" justify="space-between" key={member}>
-								<XStack gap="$sm" items="center">
-									<AccountIcon address={member} size={40} />
-									<YStack gap="$2xs">
-										<Text>Member {index + 1}</Text>
-										<Text variant="body4">{shortenAddress(member, 5)}</Text>
-									</YStack>
+						{members.map((member) => {
+							const user = userMap.get(member);
+							return (
+								<XStack items="center" justify="space-between" key={member}>
+									<XStack gap="$sm" items="center">
+										<AccountIcon address={member} size={40} />
+										<YStack gap="$2xs">
+											<Text>{user?.displayName ?? `@${user?.tag}`}</Text>
+											<Text variant="body4">
+												{shortenAddress(user?.evmAddress, 5)}
+											</Text>
+										</YStack>
+									</XStack>
+									{isSameAddress(member, params.admin as Address) ? (
+										<Text
+											py={3}
+											px={12}
+											bg="$accent2"
+											rounded="$2xl"
+											color="$accent1"
+										>
+											Admin
+										</Text>
+									) : null}
 								</XStack>
-								{isSameAddress(member, params.admin as Address) ? (
-									<Text
-										py={3}
-										px={12}
-										bg="$accent2"
-										rounded="$2xl"
-										color="$accent1"
-									>
-										Admin
-									</Text>
-								) : null}
-							</XStack>
-						))}
+							);
+						})}
 					</YStack>
 
 					<XStack
